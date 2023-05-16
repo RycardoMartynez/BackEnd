345e424381c84ff5048c4746936d5228fb1eceb9
RycardoMartynez <martynezrycardo@gmail.com>
2023-05-15 21:19:47 -0300

    void cors

--- src/main/java/com/portfolio/ram3/Ram3Application.java
+++ src/main/java/com/portfolio/ram3/Ram3Application.java
@@ -2,12 +2,27 @@ package com.portfolio.ram3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Ram3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ram3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ram3Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "https://frontendportfolioryckdev.web.app")
+                        .allowedMethods("*");
+            }
+        };
+    }
 
 }
